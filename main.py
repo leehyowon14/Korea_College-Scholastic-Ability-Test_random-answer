4a1615d9cd22c47b48b5cf6deb6837cf53103eba
leehyowon14 <57023119+leehyowon14@users.noreply.github.com>
2022-01-05 23:24:24 +0900

    변수명 알기쉽게 변경

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random, threading, os, time, datetime
 
 answer = False
-right = True
-try_ = 0
-already_fin = False
+is_answer = True
+attempt = 0
+finished = False
 all_thread_added = False
 
 class test(threading.Thread):
@@ -12,7 +12,7 @@ class test(threading.Thread):
         self.name = name
 
     def run(self):
-        global already_fin, answer, right, try_
+        global finished, answer, is_answer, attempt
         
         #모든 스레드 추가될때까지 대기
         while not all_thread_added:
@@ -20,44 +20,44 @@ class test(threading.Thread):
         
         #시뮬레이션 시작
         while not answer:
-            try_ += 1
-            right = True
+            attempt += 1
+            is_answer = True
             for i in range(1, 7):
-                if not right:
+                if not is_answer:
                     break
                 elif i == 3: #과목: 수학
-                    x_n = 22
+                    number_of_questions = 22
                     for y in range(10): #단답형
                         if random.randrange(0, 1000) != 1:
-                            print('[{}][{}]실패. 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),try_))
-                            right = False
+                            print('[{}][{}]실패. 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),attempt))
+                            is_answer = False
                             break
                 elif i == 4:#과목: 한국사
-                    x_n = 21
+                    number_of_questions = 21
                 elif i ==5:#과목: 탐구
-                    x_n = 41
+                    number_of_questions = 41
                 elif i ==6:#과목: 제2외국어
-                    x_n = 31
+                    number_of_questions = 31
                 else: #나머지 과목들
-                    x_n = 46
+                    number_of_questions = 46
                 
-                if right: #수학 단답형 틀렸을때 그냥 넘어가도록.
-                    for x in range(1, x_n):
+                if is_answer: #수학 단답형 틀렸을때 그냥 넘어가도록.
+                    for x in range(1, number_of_questions):
                         if random.randrange(1,6) != 1:
-                            if not already_fin: #성공한 후 도배 방지
-                                print('[{}][{}]실패. 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),try_))
-                            right = False
+                            if not finished: #성공한 후 도배 방지
+                                print('[{}][{}]실패. 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),attempt))
+                            is_answer = False
                             break
                 
-                if i == 6 and right:
+                if i == 6 and is_answer:
                     answer = True
                     break
             
             if answer:
                 break
         #성공
-        already_fin = True
-        print('\n\n\n[{}][{}]성공!! 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),try_))
+        finished = True
+        print('\n\n\n[{}][{}]성공!! 시도횟수: {}'.format(self.name,datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),attempt))
         print('실행시간: {:.2f}초'.format(time.time() - start_time))
         
         #모든 스레드 종료
@@ -67,9 +67,9 @@ class test(threading.Thread):
 
 #스레드 생성
 thread_num = input("스레드 수를 입력하세요: ")
-num_len = len(thread_num)
+thread_num_len = len(thread_num)
 for i in range(int(thread_num)):
-    thread_count ='0'*(num_len - len(str(i+1)))+str(i+1)
+    thread_count ='0'*(thread_num_len - len(str(i+1)))+str(i+1)
     t=test('Thread-{}'.format(thread_count))
     t.start()
     print('thread-{} started'.format(thread_count))
